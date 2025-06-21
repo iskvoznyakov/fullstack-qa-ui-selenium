6cdef2dc14e4f85d56791c35bc36a31a490192e2
Ilya Skvoznyakov <104623164+iskvoznyakov@users.noreply.github.com>
2025-06-21 15:23:32 +0400

    feat(tests): add LoginPage and smoke test for successful login (#2)

---
+++ pages/login_page.py
@@ -0,0 +1,17 @@
+from pages.base_page import BasePage
+from selenium.webdriver.common.by import By
+
+
+class LoginPage(BasePage):
+    USERNAME_INPUT = (By.ID, "user-name")
+    PASSWORD_INPUT = (By.ID, "password")
+    LOGIN_BUTTON = (By.ID, "login-button")
+    ERROR_MESSAGE = (By.CLASS_NAME, "error-message-container")
+
+    def login(self, username, password):
+        self.enter_text(self.USERNAME_INPUT, username)
+        self.enter_text(self.PASSWORD_INPUT, password)
+        self.click(self.LOGIN_BUTTON)
+
+    def is_error_displayed(self):
+        return self.is_visible(self.ERROR_MESSAGE)
