668e020383fc28592f4f4b9434fc38783d69203d
Ilya Skvoznyakov <iliaskvozniakov@mail.ru>
2025-06-25 16:33:30 +0400

    feat(logging): implement detailed logging for better debugging in UI tests
    - added utils/logger.py
    - added utils/decorators.py with @log_action decorator
    - added logger.info calls to each method in BasePage
    - applied @log_action decorator to all methods in LoginPage and InventoryPage

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,6 @@
 from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
+from utils.decorators import log_action
 
 
 class LoginPage(BasePage):
@@ -8,13 +9,16 @@ class LoginPage(BasePage):
     LOGIN_BUTTON = (By.ID, "login-button")
     ERROR_MESSAGE = (By.CLASS_NAME, "error-message-container")
 
+    @log_action
     def login(self, username, password):
         self.enter_text(self.USERNAME_INPUT, username)
         self.enter_text(self.PASSWORD_INPUT, password)
         self.click(self.LOGIN_BUTTON)
 
+    @log_action
     def is_error_displayed(self):
         return self.is_visible(self.ERROR_MESSAGE)
 
+    @log_action
     def get_error_text(self):
         return self.find(self.ERROR_MESSAGE).text
