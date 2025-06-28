76ece04d61ede3e12a7537be8d93605c04e948d3
iskvoznyakov <iliaskvozniakov@mail.ru>
2025-06-28 15:30:15 +0400

    ci: add GitHub Actions workflow for UI tests

--- conftest.py
+++ conftest.py
@@ -9,7 +9,7 @@ import allure
 @pytest.fixture(scope="function")
 def driver():
     options = Options()
-    #options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--incognito")
     options.add_argument("--window-size=1920,1080")
 
